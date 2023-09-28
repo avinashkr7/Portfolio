5fa3ee98be36935a6222d9614e75a1418d575155
Avinash Kumar <118923593+avinashkr7@users.noreply.github.com>
2023-09-29 00:05:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let sections = document.querySeIectorA11('section');
+let navLinks = document. querySe1ectorA11('header nav a');
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id= sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+    navLinks.forEach(links => {
+    links.classList.remove('active');
+    document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+      });
+    };
+  });
+};
